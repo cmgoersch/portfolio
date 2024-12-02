fdded41175f70fd07d8e57d4ad6de144de725f1e
cmgoersch <goersch@madways.de>
2024-12-02 16:12:38 +0200

    more content

---
+++ menu.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("overlay-menu").style.display = "flex";
+  }
+  
+  function closeMenu() {
+    document.getElementById("overlay-menu").style.display = "none";
+  }
