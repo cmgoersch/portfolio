ce76271b69df2eecdc0a7c9fb871897ac9e43ecb
cmgoersch <goersch@madways.de>
2025-05-14 14:35:07 +0200

    fav icon

---
+++ server.js
@@ -0,0 +1,16 @@
+    //  const express = require('express');
+    //  const path = require('path');
+    //  const app = express();
+
+    //  // Statische Dateien bereitstellen
+    //  app.use(express.static(path.join(__dirname, 'public')));
+
+    //  // Beispielroute
+    //  app.get('/', (req, res) => {
+    //      res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    //  });
+
+    //  app.listen(3000, () => {
+    //      console.log('Server läuft auf http://localhost:3000');
+    //  });
+     
